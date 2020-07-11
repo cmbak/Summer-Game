0766a6cf0df186b2b40a5f64ad785d40c2afbeb6
Chikara M <chikmii@gmail.com>
2020-07-11 13:57:36 +0100

    Added horizontal movement for player

--- Summer Game/Assets/Scripts/PlayerMovement.cs
+++ Summer Game/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += horizontal * Time.deltaTime;
+
     }
 }
