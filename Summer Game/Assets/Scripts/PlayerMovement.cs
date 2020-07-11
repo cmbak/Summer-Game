78a8a909cabdf9491e1a939546e13b01100c75f0
Chikara M <chikmii@gmail.com>
2020-07-11 14:40:18 +0100

    Added script functionality that flips character depending on movement direction

--- Summer Game/Assets/Scripts/PlayerMovement.cs
+++ Summer Game/Assets/Scripts/PlayerMovement.cs
@@ -19,5 +19,16 @@ public class PlayerMovement : MonoBehaviour
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += horizontal * Time.deltaTime * speed;
 
+        Vector3 characterScale = transform.localScale;
+        if(Input.GetAxis("Horizontal") > 0) //If moving right
+        {
+            characterScale.x = 1;
+        }
+        else if (Input.GetAxis("Horizontal") < 0) //Moving left
+        {
+            characterScale.x = -1;
+        }
+
+        transform.localScale = characterScale;
     }
 }
