57b29f7ab1a0a953e3003271f04f170a6f8b6b5e
Chikara M <chikmii@gmail.com>
2020-07-13 11:14:32 +0100

    Added functionality to CameraFollow script so that the main camera follows the player

--- Summer Game/Assets/Scripts/CameraFollow.cs
+++ Summer Game/Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public float offsetX = 2;
+    private Transform playerTransform; //player movement
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void LateUpdate()
     {
-        
+        //Current position of camera - can't modify position of camera directly
+        Vector3 tempPosition = transform.position;  
+        //Set camera's x to the x of the player
+        tempPosition.x = playerTransform.position.x + offsetX;  
+
+        transform.position = tempPosition;
     }
 }
