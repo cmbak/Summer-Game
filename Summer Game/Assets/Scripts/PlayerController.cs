801b4192bcda661a43037921033ddab5390a5fc8
Chikara M <chikmii@gmail.com>
2020-07-13 12:19:26 +0100

    Renamed PlayerMovement script to PlayerController

---
+++ Summer Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    //public LayerMask groundLayer;
+    //public RigidBody rb;
+    public bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         rb = GetComponent<Rigidbody2D>();
+         sprite = GetComponent<SpriteRenderer>();
+         isGrounded = true; //change if needed
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += horizontal * Time.deltaTime * speed;
+
+        Vector3 characterScale = transform.localScale;
+        if(Input.GetAxis("Horizontal") > 0) //If moving right
+        {
+            characterScale.x = 1;
+        }
+        else if (Input.GetAxis("Horizontal") < 0) //Moving left
+        {
+            characterScale.x = -1;
+        }
+
+        transform.localScale = characterScale;
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
