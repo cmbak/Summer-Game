da50b788c9bf607768b9cc18bab97eec58625036
Chikara M <chikmii@gmail.com>
2020-07-15 16:27:28 +0100

    Cleaned up script

--- Summer Game/Assets/Scripts/PlayerController.cs
+++ Summer Game/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,16 @@ public class PlayerController : MonoBehaviour
 {
 
     public int amountOfDoubleJumps;
-    public bool canDoubleJump; //change to private after
+    public bool canDoubleJump;
     public float speed;
-    public int amountOfCoins;
     public float jumpForce;
     private Rigidbody2D rb;
-    private SpriteRenderer sprite;
     public bool isGrounded = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
         isGrounded = true; 
         canDoubleJump = true;
         amountOfDoubleJumps = 1;
@@ -31,7 +28,7 @@ public class PlayerController : MonoBehaviour
         transform.position += horizontal * Time.deltaTime * speed;
 
         Vector3 characterScale = transform.localScale;
-        if(Input.GetAxis("Horizontal") > 0) //If moving right
+        if(Input.GetAxis("Horizontal") > 0) //Moving right
         {
             characterScale.x = 1;
         }
@@ -46,7 +43,7 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         } 
-        else if (Input.GetButtonDown("Jump") && isGrounded == false && canDoubleJump == true && amountOfDoubleJumps > 0) //simplify /split conditionals into multiple if statements
+        else if (Input.GetButtonDown("Jump") && isGrounded == false && canDoubleJump == true && amountOfDoubleJumps > 0)
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             amountOfDoubleJumps --;
