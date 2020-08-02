94b27e0135c47c31d77db20d73925d900a3b9adf
Chikara M <chikmii@gmail.com>
2020-08-02 22:05:16 +0100

    Health Bar's fill is affected by the gradient

--- Summer Game/Assets/Scripts/HealthBar.cs
+++ Summer Game/Assets/Scripts/HealthBar.cs
@@ -8,14 +8,18 @@ public class HealthBar : MonoBehaviour
 
     public Slider slider;
     public Gradient gradient;
+    public Image fill;
     public void SetMaxHealth(int health) //Used to set Max at start of game
     {
         slider.maxValue = health;
         slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
