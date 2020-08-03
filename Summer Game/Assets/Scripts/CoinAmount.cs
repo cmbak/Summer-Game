fa6e53d7e6f8cf4023d705df16c7b40be49b8aff
Chikara M <chikmii@gmail.com>
2020-08-03 16:30:34 +0100

    Coin counter now shows actual amounts of coins

--- Summer Game/Assets/Scripts/CoinAmount.cs
+++ Summer Game/Assets/Scripts/CoinAmount.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CoinAmount : MonoBehaviour
 {
+    public TextMeshProUGUI coinText;
+    private PlayerController Player;
+    private int amountOfCoins;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        coinText = coinText.GetComponent<TextMeshProUGUI>(); //Looks for the TMPUGUI component in the coinText object
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      coinText.SetText($"x {Player.Coins.ToString()}");  
     }
 }
