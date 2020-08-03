897612d615b44d4ccff06d8c16debd18af61cab2
Chikara M <chikmii@gmail.com>
2020-08-03 15:00:08 +0100

    Imported TMP Essentials, Examples and Extras

--- Summer Game/Assets/Scripts/Hearts.cs
+++ Summer Game/Assets/Scripts/Hearts.cs
@@ -7,15 +7,16 @@ public class Hearts : MonoBehaviour
 {
     public Sprite[] HeartSprites;
     public Image HeartUI;
+    private PlayerController player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HeartUI.sprite = HeartSprites[player.Lives];
     }
 }
