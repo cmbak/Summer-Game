f2e39cd577ec35d25e722da775c5aea8f39af38e
Chikara M <chikmii@gmail.com>
2020-08-14 12:14:42 +0100

    Created pause menu with working pause key shortcut

--- Summer Game/Assets/Scripts/PauseMenu.cs
+++ Summer Game/Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
     void Start()
     {
-        
+        Resume();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+       if (Input.GetKeyDown(KeyCode.Escape))
+       {
+           if (gameIsPaused)
+           {
+               Resume();//Resume game
+           }
+           else
+           {
+               Pause();
+               //Pause game
+               //gameIsPaused = true;
+           }
+       }
+    }
+
+    private void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.5f; //change to 0 after
+        gameIsPaused = true;    
     }
 }
