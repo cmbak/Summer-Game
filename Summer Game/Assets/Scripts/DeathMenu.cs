5eab79413c70d8a7d7bb6cd62621db57e46d56eb
Chikara M <chikmii@gmail.com>
2020-08-21 17:53:26 +0100

    Death menu now shows once player has no lives or hp left (previously selected wrong game object)

--- Summer Game/Assets/Scripts/DeathMenu.cs
+++ Summer Game/Assets/Scripts/DeathMenu.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeathMenu : MonoBehaviour
 {
+    public GameObject deathMenuUI;
+    private PlayerController player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        deathMenuUI.SetActive(false);
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.Lives == 0 && player.HP == 0)
+        {
+            ShowMenu();
+            Debug.Log("just die");
+        }
+    }
+
+    private void ShowMenu()
+    {
+        deathMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Debug.Log("Die");
     }
 }
