4218e0e86c6d030313bd0e35031f0e97ccae7ff3
cmbak <chikmii@gmail.com>
2023-10-27 12:35:30 +0100

    Switched platform to WebGL

--- Summer Game/Assets/RuleTile/RuleTileEditor.cs
+++ Summer Game/Assets/RuleTile/RuleTileEditor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3eb71b6a16be5ec2ae40fae4688a9910fddf9afe2cb357ae1a94eb0ae520eff7
-size 33692
+oid sha256:5088c0d1cc14011f5bbd7e71dbe16f181b16443e268fb83181c3712c2a7af6f9
+size 33716
---
+++ Summer Game/Library/webgl_cache/linkresult_wasm_57FEE65454728ACE16CBC1EC920CE386/build.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b4ae2b60a1101549e4ba79ff7d5bd8ef1a8e13f5adbd36095bb300eb78544f7
+size 477883
