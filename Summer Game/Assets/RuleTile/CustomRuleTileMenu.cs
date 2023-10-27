541b3cb7479a102ab98bfe3f116b662f3e9958d1
cmbak <chikmii@gmail.com>
2023-10-27 14:27:08 +0100

    Changed to webgl again

---
+++ Summer Game/Assets/RuleTile/CustomRuleTileMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:90f586fcb9ef914621487576d5c61c682a17d5a3e68d985bc4e463f70f631060
+size 426
---
+++ Summer Game/Assets/RuleTile/RuleTileEditor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f44cbf80cd97dbc5dfcbb5a759e9e0dc48ed060e7f11e639aa5fb3ac740b8602
+size 33715
